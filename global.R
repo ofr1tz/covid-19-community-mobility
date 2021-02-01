d8fce432e76913d193becbc7386f2dd3324730b8
ofr1tz <ofr1tz@yahoo.de>
2021-02-01 17:38:23 +0000

    remove data update from global

--- global.R
+++ global.R
@@ -16,32 +16,6 @@ conflict_prefer("filter", "dplyr")
 # source shared variables used by update_data cron job and app
 source("shared_vars.R")
 
-
-# if(!dir.exists("data")) { dir.create("data") }
-# if(!file.exists(file) | as.Date(file.info(file)$mtime) != Sys.Date()) {
-# 	
-# 	temp <- try(read_csv(url), silent = T)
-# 	if(class(temp) != "try-error") {
-# 		temp %>%
-# 			filter(is.na(sub_region_1)) %>%
-# 			rename(iso_a2 = country_region_code, country = country_region) %>%
-# 			select(-sub_region_1, -sub_region_2, -metro_area) %>%
-# 			gather("category", "mobility", -iso_a2, -country, -iso_3166_2_code, -census_fips_code, -date) %>%
-# 			mutate(
-# 				category = str_to_title(
-# 					str_replace_all(str_replace(category, "_percent_change_from_baseline", ""), "_", " ")
-# 				),
-# 				bin = cut(
-# 					mobility, 
-# 					breaks = c(seq(-100, 100, 20), Inf), 
-# 					labels = labels, 
-# 					right = F
-# 				)
-# 			) %>%
-# 			saveRDS(file)
-# 	}
-# }
-
 dat <- readRDS(file)
 
 data_attribution = paste(
