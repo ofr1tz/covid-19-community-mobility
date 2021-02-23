fdd93a30388ddd33d129d18bd88add6157e76a1b
ofr1tz <ofr1tz@yahoo.de>
2021-02-23 18:05:15 +0000

    explicit col_types, deselect place_id column

--- update_data.R
+++ update_data.R
@@ -8,12 +8,12 @@ source("shared_vars.R")
 
 if(!dir.exists("data")) { dir.create("data") }
 
-temp <- try(read_csv(url), silent = T)
+temp <- try(read_csv(url, col_types = "ccccccccDiiiiii"), silent = T)
 if(class(temp) != "try-error") {
 	temp %>%
 		filter(is.na(sub_region_1)) %>%
 		rename(iso_a2 = country_region_code, country = country_region) %>%
-		select(-sub_region_1, -sub_region_2, -metro_area) %>%
+		select(-sub_region_1, -sub_region_2, -metro_area, -place_id) %>%
 		gather("category", "mobility", -iso_a2, -country, -iso_3166_2_code, -census_fips_code, -date) %>%
 		mutate(
 			category = str_to_title(
